1cbfd65225c414717603068e3b90008701f13eca
SpeedataGroup <shiyao.wang@speedatagroup.com>
2019-04-15 13:56:20 +0800

    Printer

---
+++ app/src/main/java/utils/LinkContactActivity.java
@@ -0,0 +1,16 @@
+package utils;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.speedata.print.R;
+
+
+public class LinkContactActivity extends Activity {
+ 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_linkcontact);
+	}
+}
